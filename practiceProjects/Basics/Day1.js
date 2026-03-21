d26d5508ca713105c9758e1a02cf58fa063dd300
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-03-21 23:37:37 +0530

    Created some practice projects in notebook & on code editor

---
+++ practiceProjects/Basics/Day1.js
@@ -0,0 +1,26 @@
+// Discount Calculator
+const userPrice = 125;
+const discountedPrice = 10/100 * userPrice;
+const TotalpRice = 100
+
+if (userPrice >= 200){
+    console.log(TotalpRice);
+} else _{
+    console.log()
+}
+// Age Eligibility Checker
+// Simple Login Checker
+// Temperature status
+// Even or Odd Checker
+// Simple Interest Calculator
+// Greater Number
+// Password Strength Checker
+// Cart Total
+// Intro to Loop
+
+
+
+
+
+
+
