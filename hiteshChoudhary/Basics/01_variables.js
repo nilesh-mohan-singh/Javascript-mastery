52b419e862b17261db68c1ec4f654b7ed65d3da8
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-03-21 22:41:04 +0530

    Added JS practice files and folders for practice

---
+++ hiteshChoudhary/Basics/01_variables.js
@@ -0,0 +1,6 @@
+a = 2;
+let a = 2;
+// const a = 3;
+// var a = 1;
+
+console.log(a);
---
+++ hiteshChoudhary/Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+// Primitive - string, number, boolean, undefined, null, bigint, symbol, 
+// Reference - array, function, object
+
+let fullname = `Nilesh Mohan Singh`;
+const dob = 10;
+let isdob = true;
+let x;
+let y = null;
+let value = BigInt("123");
+let s = Symbol("Hello");
+
+console.log(fullname);
+console.log(dob);
+console.log(isdob);
+console.log(x);
+console.log(y);
+console.log(value);
+console.log(s);
+
+
+
