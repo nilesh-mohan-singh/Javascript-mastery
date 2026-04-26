9327f914e6dd6480c45a9c39ca2977e76f42fcd2
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-26 18:46:21 +0530

    Conditionals and Loops overview

--- 01-core-js/09-controls/01.js
+++ 01-core-js/09-controls/01.js
@@ -8,4 +8,64 @@ function hello(){
 
 
 
+// Conditionals
+// If Statement
+// If Else Statement
+// If Else If Else Statement
+
+// If Statement
+let num = 1;
+if (num === 2){
+    // console.log("Hi, Nilesh!");
+}
+
+if (num === 2){
+    // console.log(num, " is true")
+} else {
+    // console.log(num, " is wrong")
+}
+
+
+
+if (num === 2){
+    // console.log(num, " is true")
+} else if(num <= 1) {
+    // console.log(num, " is too low")
+} else {
+    // console.log(num, " is wrong")
+}
+
+
+
+
+// Loops
+// for Loop 
+// while loop
+// do while loop
+// for of loop 
+// for in loop
+
+
+// for Loop 
+for (let num = 2; num < 200; num *= 3) {
+    console.log(num);
+};
+
+
+// while loop
+let n = 2;
+while (n < 10) {
+    console.log(`Value of n is ${n}`);
+    n = n + 2;
+}
+
+
+// do while loop
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score ++;
+} while (score < 10);
+
 
