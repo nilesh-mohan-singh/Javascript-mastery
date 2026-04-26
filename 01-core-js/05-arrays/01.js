cbd26d0ac995b1f986b1131c23e29b303227b3f0
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-04-26 08:35:45 +0530

    Arrays Overview and written advanced topics for practice

---
+++ 01-core-js/05-arrays/01.js
@@ -0,0 +1,51 @@
+
+// Create Array
+// Array Length
+// Array Indices
+
+// Array Methods
+// arr.push(),arr.pop(), arr.unshift(), arr.shift(), slice, splice()
+// foreach()
+// Spread & Rest Operators
+
+
+/*
+Iteration & Transformation (MOST IMPORTANT)
+Searching & Checking
+Adding & Removing
+Immutable-friendly (VERY IMPORTANT in React/modern JS)
+Sorting & Ordering
+Utility (used often)
+*/
+
+
+// shallow and deep copy
+// Array Destructuring
+// Immutable Patterns
+// Generator
+// Typed Arrays
+// Data Transformation
+// Array Like Objects
+
+
+/*
+
+js logic building - callbacks, HOF, closures, Recursion
+
+js finished - 26
+
+Html and Css - 27
+
+node js finished - 28
+
+react finished - 30
+
+
+*/
+
+
+const myArr = [0, 1, 2, 3, 4]
+const myHeros = ["shaktiman", "naagraaj"]
+
+const arr = new Array(1, 2, 3, 4)
+console.log(arr)
