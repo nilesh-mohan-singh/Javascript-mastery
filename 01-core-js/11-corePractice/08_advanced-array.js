ce0b2708c61185ba31550d66616229c67ec504a8
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-05-16 23:42:37 +0530

    Array related practie here and mostly in notebook

---
+++ 01-core-js/11-corePractice/08_advanced-array.js
@@ -0,0 +1,16 @@
+// forEach
+// map
+// filter
+// reduce
+// custom callbacks
+
+// sort()
+// find()
+
+let numbers = [1, 2, 3, 4, 5] 
+let number = numbers.forEach((num) => {
+    console.log(num)
+})
+
+
+
