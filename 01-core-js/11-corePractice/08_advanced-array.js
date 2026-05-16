0ca0c6ef702a45113e3e20bca77348098e94eca5
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-05-16 23:49:51 +0530

    practiced here around basic array

--- 01-core-js/11-corePractice/08_advanced-array.js
+++ 01-core-js/11-corePractice/08_advanced-array.js
@@ -12,5 +12,17 @@ let number = numbers.forEach((num) => {
     console.log(num)
 })
 
+// numbers.forEach(function (num) {
+//     console.log(num);
+// })
+
+// numbers.forEach(function num (num) {
+//     console.log(num);
+// })
 
 
+
+let allNumbers = [1, 2, 3 , 4, 5]
+let doubled = numbers.map ((num) => {
+    console.log(doubled);
+})
