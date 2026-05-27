a0406093a45785e9019f6867ea529cf45dc2ef16
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-05-27 07:40:01 +0530

    Hello World Variations

---
+++ 05-mini-projects/01-basics/01-helloworld.js
@@ -0,0 +1,20 @@
+console.log("hello World!");
+console.log("Hello WORLD!");
+console.log("HELLO WORLD!");
+
+
+let greetingName = "Nilesh";
+greetingName = "Hello " + greetingName + "!";
+console.log(greetingName);
+
+
+let fullName = "Nilesh";
+let greeting = `Hello ${fullName.toUpperCase()}!`;
+// greeting = greeting.toLowerCase();
+console.log(greeting);
+
+
+let fullGreeting = `Hi, ${greetingName}`
+fullGreeting = fullGreeting.toUpperCase();
+fullGreeting = fullGreeting.toLowerCase();
+console.log(fullGreeting);
