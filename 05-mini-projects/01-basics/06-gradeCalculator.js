a77b815fa584295a37763851af344ea2a13c966a
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-05-27 09:01:32 +0530

    grade calculator

---
+++ 05-mini-projects/01-basics/06-gradeCalculator.js
@@ -0,0 +1,17 @@
+let totalNum = 19;
+if (totalNum >= 90 && totalNum <= 100){
+    console.log(totalNum, "is grade A")
+} else if (totalNum >= 80 && totalNum <= 89) {
+    console.log(totalNum, "is grade B")
+} else if (totalNum >= 70 && totalNum <= 79) {
+    console.log(totalNum, "is grade C")
+} else if (totalNum >= 60 && totalNum <= 69) {
+    console.log(totalNum, "is grade D ")
+} else if (totalNum >= 50 && totalNum <= 59) {
+    console.log(totalNum, "is grade E ")
+} else if (totalNum <= 50) {
+    console.log("Fail");
+} else {
+    console.log("Invalid Number")
+}
+
