6d20d40a6d75c1990113e0132583a853deed7b16
Nilesh Mohan Singh <singhnileshmohan@gmail.com>
2026-05-27 08:11:14 +0530

    even odd checker with dynamic input

---
+++ 05-mini-projects/01-basics/02-evenOddCheck.js
@@ -0,0 +1,29 @@
+// let num = 24;
+// if (num % 2 === 0) {
+//     console.log(`${num} is even.`)
+// } else {
+//     console.log(`${num} is odd.`)
+// }
+
+
+let userNumber = prompt(`Enter the number !`);
+let newScore = Number(userNumber);
+console.log(newScore);
+
+
+let userScore = Number(prompt(`Check the number is even or odd:`));
+// console.log(userScore);
+if (userScore % 2 === 0) {
+    console.log(`${userScore} is even number`)
+} else {
+    console.log(`${userScore} is even number`)
+}
+
+
+// let score = "33"
+// let valScore = Number(score);
+// console.log(typeof valScore);
+// console.log(typeof (valScore));
+
+
+
